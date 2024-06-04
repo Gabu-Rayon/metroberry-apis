76648257511f6597e7a2fc04104b882ed35ac100
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-06-04 10:39:05 +0300

    First Commit of development branch

---
+++ database/migrations/2024_06_04_065738_0001_01_01_00003_create_customers_table.php.php
@@ -0,0 +1,35 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('username');
+            $table->foreignId('organisation_id')->constrained('organisations');
+            $table->string('customer_organisation_code');
+            $table->string('contact');
+            $table->string('home_address');
+            $table->string('password');
+            $table->string('avatar')->nullable(); 
+            $table->boolean('is_email_verified')->default(false); 
+            $table->boolean('is_contact_verified')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
