a19d9a6bd40f129fee4275faa3cf60cb23511350
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-06-04 17:21:29 +0300

    commit CustomerLogin Api

--- database/migrations/2024_06_04_065738_0001_01_01_00003_create_customers_table.php.php
+++ database/migrations/2024_06_04_065738_0001_01_01_00003_create_customers_table.php.php
@@ -13,6 +13,7 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->string('username');
+            $table->string('email')->unique();
             $table->foreignId('organisation_id')->constrained('organisations');
             $table->string('customer_organisation_code');
             $table->string('contact');
