56df24d794fad1883f1e8b08b6939bd12028fe76
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-06-05 17:28:46 +0300

    Commit Changes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Spatie\Permission\Contracts\Role;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\VehicleController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
