0ad23cc194b84fb6f48192a55afbaba0f4448674
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-06-05 17:23:18 +0300

    Commit changes made

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,5 @@ Route::post('routes', [AddRouteController::class, 'store'])->middleware(['auth:s
 
 Route::get('organisation', [OrganisationController::class, 'index'])->middleware(['auth:sanctum', 'can:view organisations']);
 Route::post('organisation', [OrganisationController::class, 'store'])->middleware(['auth:sanctum', 'can:create organisation']);
+
+Route::post('organisation/addCustomer', [OrganisationController::class, 'addCustomer'])->middleware(['auth:sanctum', 'can:create a customer']);
