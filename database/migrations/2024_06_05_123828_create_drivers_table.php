a284b6fde2d6dae12f75398a83bd520e6024593e
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-06-06 14:42:36 +0300

    Commit change made on migrations

--- database/migrations/2024_06_05_123828_create_drivers_table.php
+++ database/migrations/2024_06_05_123828_create_drivers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('drivers', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('vehicle_id')->nullable();            
             $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
         });
     }
