10744c41345cf7599f3cc07aafa52ef2d1d84eb1
10-menachi <bobitlmr188221@spu.ac.ke>
2024-06-06 10:12:19 +0300

    Make changes

--- app/Models/Organisation.php
+++ app/Models/Organisation.php
@@ -10,6 +10,7 @@ class Organisation extends Model
     use HasFactory;
     protected $table = 'organisations';
     protected $fillable = [
+        'user_id',
         'status',
         'created_by',
     ];
