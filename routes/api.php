bfb239c5132a8e7c9c30b860c578be9b53571ca1
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-06-06 09:50:59 +0300

    Commit Changes

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::get('vehicles/{vehicle}', [VehicleController::class, 'show'])->middleware
 Route::post('vehicles', [VehicleController::class, 'store'])->middleware(['auth:sanctum', 'can:create vehicles']);
 Route::put('vehicles/{vehicle}', [VehicleController::class, 'update'])->middleware(['auth:sanctum', 'can:edit vehicle']);
 Route::delete('vehicles/{vehicle}', [VehicleController::class, 'destroy'])->middleware(['auth:sanctum', 'can:delete vehicle']);
+Route::post('assign-driver/{vehicle}', [VehicleController::class, 'assign_driver'])->middleware(['auth:sanctum', 'can:assign driver']);
 
 Route::get('organisation', [OrganisationController::class, 'index'])->middleware(['auth:sanctum', 'can:view organisation']);
 Route::post('organisation', [OrganisationController::class, 'store'])->middleware(['auth:sanctum', 'can:create organisation']);
