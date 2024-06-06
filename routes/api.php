70bb1fa1cbb3b8301bd5d16e1c0a8fee30773a79
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-06-06 09:40:54 +0300

    Commit Changes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Controllers\AddRouteController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerLoginController;
+use App\Http\Controllers\OrganisationController;
+use App\Http\Controllers\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Spatie\Permission\Contracts\Role;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\VehicleController;
-use App\Http\Controllers\AddRouteController;
-use App\Http\Controllers\OrganisationController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -28,21 +30,6 @@ Route::delete('vehicles/{vehicle}', [VehicleController::class, 'destroy'])->midd
 
 Route::get('organisation', [OrganisationController::class, 'index'])->middleware(['auth:sanctum', 'can:view organisation']);
 Route::post('organisation', [OrganisationController::class, 'store'])->middleware(['auth:sanctum', 'can:create organisation']);
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomerLoginController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::post('customerLogin', [CustomerLoginController::class,'customerLogin']);
 
