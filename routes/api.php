5df0133df468b6e601cc251549931943e09b272a
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-06-06 14:49:43 +0300

    Commit

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AddRouteController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\CustomerLoginController;
 use App\Http\Controllers\DriverController;
 use App\Http\Controllers\OrganisationController;
 use App\Http\Controllers\VehicleController;
@@ -33,8 +32,6 @@ Route::post('assign-driver/{vehicle}', [VehicleController::class, 'assign_driver
 
 Route::get('organisation', [OrganisationController::class, 'index'])->middleware(['auth:sanctum', 'can:view organisation']);
 Route::post('organisation', [OrganisationController::class, 'store'])->middleware(['auth:sanctum', 'can:create organisation']);
-
-Route::post('customerLogin', [CustomerLoginController::class,'customerLogin']);
 
 // Route::get('customerLogin', [CustomerLoginController::class, 'index'])->middleware(['auth:sanctum', 'can:can login']);
 // Route::post('customerLogin', [CustomerLoginController::class, 'store'])->middleware(['auth:sanctum', 'can:can view login page']);
