73ca4e61a6dce7a363fd49e6f416fc33c9d4ce39
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-06-18 16:21:01 +0300

    Commit changes

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -89,78 +89,78 @@ class DriverController extends Controller
     //     }
     // }
 
-   public function store(Request $request)
-{
-    try {
-        $creator = User::find(Auth::id());
+    public function store(Request $request)
+    {
+        try {
+            $creator = User::find(Auth::id());
 
-        Log::info('CREATOR');
-        Log::info($creator);
+            Log::info('CREATOR');
+            Log::info($creator);
 
-        $data = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users,email',
-            'phone' => 'required|string',
-            'password' => 'required|integer',
-           'organisation_id' => 'required|string',
-            'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'national_id_no' => 'required|string|unique:drivers',
-            'national_id_avatar_front' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'national_id_avatar_behind' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'sex' => 'required|string',
-            'date_of_birth' => 'required|date_format:Y-m-d',
-            'driving_license_no' => 'required|string|unique:drivers',
-            'driving_license_date_issued' => 'required|date_format:Y-m-d',
-            'driving_license_date_expiry' => 'required|date_format:Y-m-d',
-            'dl_county_of_residence' => 'required|string',
-            'dl_avatar_front' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'dl_avatar_behind' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-        ]);
+            $data = $request->validate([
+                'name' => 'required|string',
+                'email' => 'required|email|unique:users,email',
+                'phone' => 'required|string',
+                'password' => 'required|integer',
+                'organisation_id' => 'required|string',
+                'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+                'national_id_no' => 'required|string|unique:drivers',
+                'national_id_avatar_front' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+                'national_id_avatar_behind' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+                'sex' => 'required|string',
+                'date_of_birth' => 'required|date_format:Y-m-d',
+                'driving_license_no' => 'required|string|unique:drivers',
+                'driving_license_date_issued' => 'required|date_format:Y-m-d',
+                'driving_license_date_expiry' => 'required|date_format:Y-m-d',
+                'dl_county_of_residence' => 'required|string',
+                'dl_avatar_front' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+                'dl_avatar_behind' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            ]);
 
-        $avatarPath = null;
-        if ($request->hasFile('avatar')) {
-            $avatarPath = $request->file('avatar')->store('DriversAvatars', 'public');
+            $avatarPath = null;
+            if ($request->hasFile('avatar')) {
+                $avatarPath = $request->file('avatar')->store('DriversAvatars', 'public');
+            }
+
+            $user = User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'phone' => $data['phone'],
+                'password' => bcrypt($data['password']),
+                'avatar' => $avatarPath,
+            ]);
+
+            $driver = Driver::create([
+                'user_id' => $user->id,
+                'organisation_id' => $data['organisation_id'],
+                'created_by' => Auth::id(),
+                'status' => 'inactive',
+                'national_id_no' => $data['national_id_no'],
+                'national_id_avatar_front' => $request->file('national_id_avatar_front')->store('DriversIDAvatars', 'public'),
+                'national_id_avatar_behind' => $request->file('national_id_avatar_behind')->store('DriversIDAvatars', 'public'),
+                'sex' => $data['sex'],
+                'date_of_birth' => $data['date_of_birth'],
+                'driving_license_no' => $data['driving_license_no'],
+                'driving_license_date_issued' => $data['driving_license_date_issued'],
+                'driving_license_date_expiry' => $data['driving_license_date_expiry'],
+                'dl_county_of_residence' => $data['dl_county_of_residence'],
+                'dl_avatar_front' => $request->file('dl_avatar_front')->store('DriversLicenseAvatars', 'public'),
+                'dl_avatar_behind' => $request->file('dl_avatar_behind')->store('DriversLicenseAvatars', 'public'),
+            ]);
+
+            return response()->json([
+                'message' => 'Driver created successfully',
+                'driver' => $driver
+            ], 201);
+        } catch (Exception $e) {
+            Log::error('CREATE DRIVER ERROR');
+            Log::error($e);
+            return response()->json([
+                'message' => 'An error occurred',
+                'error' => $e->getMessage()
+            ], 500);
         }
-
-        $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'phone' => $data['phone'],
-            'password' => bcrypt($data['password']),
-            'avatar' => $avatarPath,
-        ]);
-
-        $driver = Driver::create([
-            'user_id' => $user->id,
-            'organisation_id' => $data['organisation_id'],
-            'created_by' => Auth::id(),
-            'status' => 'inactive',
-            'national_id_no' => $data['national_id_no'],
-            'national_id_avatar_front' => $request->file('national_id_avatar_front')->store('DriversIDAvatars', 'public'),
-            'national_id_avatar_behind' => $request->file('national_id_avatar_behind')->store('DriversIDAvatars', 'public'),
-            'sex' => $data['sex'],
-            'date_of_birth' => $data['date_of_birth'],
-            'driving_license_no' => $data['driving_license_no'],
-            'driving_license_date_issued' => $data['driving_license_date_issued'],
-            'driving_license_date_expiry' => $data['driving_license_date_expiry'],
-            'dl_county_of_residence' => $data['dl_county_of_residence'],
-            'dl_avatar_front' => $request->file('dl_avatar_front')->store('DriversLicenseAvatars', 'public'),
-            'dl_avatar_behind' => $request->file('dl_avatar_behind')->store('DriversLicenseAvatars', 'public'),
-        ]);
-
-        return response()->json([
-            'message' => 'Driver created successfully',
-            'driver' => $driver
-        ], 201);
-    } catch (Exception $e) {
-        Log::error('CREATE DRIVER ERROR');
-        Log::error($e);
-        return response()->json([
-            'message' => 'An error occurred',
-            'error' => $e->getMessage()
-        ], 500);
     }
-}
 
     /**
      * Display the specified resource.
