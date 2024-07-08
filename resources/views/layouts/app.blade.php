68e348f617bf5aee73db39b8f260244af0d41b7e
10-menachi <bobitlmr188221@spu.ac.ke>
2024-07-08 12:00:28 +0300

    Separate Layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,116 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!doctype html>
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    @csrf
+    <meta name="base-url" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <title>
+        @yield('title')
+    </title>
+    <meta name="description" content="Login" />
+    <link rel="canonical" href="login.html" />
+    <meta name="robots" content="all" />
+    <meta property="og:description" content="Login" />
+    <meta property="og:title" content="Login" />
+    <meta property="og:url" content="login.html" />
+    <meta property="og:type" content="WebPage" />
+    <meta property="og:site_name" content="vms-laravel" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="admin-assets/img/favicon.png?v=1" />
+    <link href="{{ asset('admin-assets/vendor/bootstrap/css/bootstrap.min.css?v=1')}}"
+    rel="stylesheet" />
+    <link
+      href="{{ asset('admin-assets/vendor/metisMenu/metisMenu.min.css?v=1') }}"
+      rel="stylesheet"
+    />
+    <link
+      href="{{ asset('nanopkg-assets/vendor/datatables/dataTables.bootstrap4.min.css?v=1') }}"
+      rel="stylesheet"
+    />
+    <link
+      href="{{ asset('admin-assets/vendor/typicons/src/typicons.min.css?v=1') }}"
+      rel="stylesheet"
+    />
+    <link
+      href="{{ asset('admin-assets/vendor/themify-icons/themify-icons.min.css?v=1') }}"
+      rel="stylesheet"
+    />
+    <link
+      href="{{ asset('admin-assets/vendor/material_icons/materia_icons.css?v=1') }}"
+      rel="stylesheet"
+    />
+    <link
+      href="{{ asset('admin-assets/vendor/emojionearea/dist/emojionearea.min.css?v=1') }}"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href=" {{ asset('nanopkg-assets/vendor/highlight/highlight.min.css?v=1') }}"
+    />
+    <link
+      href="{{ asset('nanopkg-assets/vendor/sweetalert2/sweetalert2.min.css?v=1') }}"
+      rel="stylesheet"
+    />
+    <link
+      href="{{ asset('nanopkg-assets/vendor/fontawesome-free-6.3.0-web/css/all.min.css?v=1') }}"
+      rel="stylesheet"
+    />
+    <link
+      href="{{ asset('nanopkg-assets/vendor/bootstrap-icons/css/bootstrap-icons.min.css?v=1') }}"
+      rel="stylesheet"
+    />
+    <link
+      href="{{ asset('nanopkg-assets/vendor/toastr/build/toastr.min.css?v=1') }}"
+      rel="stylesheet"
+    />
+    <link href="{{ asset('nanopkg-assets/css/arrow-hidden.min.css?v=1') }}" rel="stylesheet" />
+    <link href="{{ asset('nanopkg-assets/css/custom.min.css?v=1') }}" rel="stylesheet" />
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!--Start Your Custom Style Now-->
+    <link href="{{ asset('admin-assets/css/style-new.min.css?v=1') }}" rel="stylesheet" />
+    <link href="{{ asset('admin-assets/css/custom.min.css?v=1') }}" rel="stylesheet" />
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('admin-assets/scss/customStyle.min.css?v=1') }}" rel="stylesheet" />
+    <link href="{{ asset('admin-assets/css/grapData.min.css?v=1') }}" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <link rel="stylesheet" href="{{ asset('module-assets/Auth/css/login.min.css?v=1') }}" />
+  </head>
+<body class="fixed sidebar-mini">
+    <div class="main">
+        @yield('content')
+    </div>
+    <script src="{{ asset('admin-assets/vendor/jQuery/jquery.min.js?v=1') }}"></script>
+    <script src="{{ asset('admin-assets/vendor/bootstrap/js/bootstrap.bundle.min.js?v=1') }}"></script>
+    <script src="{{ asset('admin-assets/vendor/emojionearea/dist/emojionearea.min.js?v=1') }}"></script>
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <script src="{{ asset('admin-assets/vendor/metisMenu/metisMenu.min.js?v=1') }}"></script>
+    <script src="{{ asset('admin-assets/vendor/perfect-scrollbar/dist/perfect-scrollbar.min.js?v=1') }}"></script>
+    <script src="{{ asset('nanopkg-assets/vendor/sweetalert2/sweetalert2.min.js?v=1') }}"></script>
+    <script src="{{ asset('nanopkg-assets/vendor/fontawesome-free-6.3.0-web/js/all.min.js?v=1') }}"></script>
+    <script src="{{ asset('nanopkg-assets/vendor/toastr/build/toastr.min.js?v=1') }}"></script>
+    <script src="{{ asset('nanopkg-assets/vendor/axios/dist/axios.min.js?v=1') }}"></script>
+    <script src="{{ asset('cnanopkg-assets/vendor/typed.js/lib/typed.min.js?v=1') }}"></script>
+    <script src=" {{ asset('nanopkg-assets/vendor/jquery-validation-1.19.5/jquery.validate.min.js?v=1') }}"></script>
+    <script src="{{ asset('cnanopkg-assets/js/axios.init.min.js?v=1') }}"></script>
+    <script src="{{ asset('nanopkg-assets/js/arrow-hidden.min.js?v=1') }}"></script>
+    <script src="{{ asset('nanopkg-assets/js/img-src.min.js?v=1') }}"></script>
+    <script src="{{ asset('nanopkg-assets/js/delete.min.js?v=1') }}"></script>
+    <script src="{{ asset('nanopkg-assets/js/user-status-update.min.js?v=1') }}"></script>
+    <script src="{{ asset('nanopkg-assets/js/main.js?v=1') }}"></script>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+    <!--Page Scripts(used by all page)-->
+    <script src="{{ asset('admin-assets/js/sidebar.min.js?v=1') }}"></script>
+    <script src="{{ asset('dmin-assets/js/custom.min.js?v=1') }}"></script>
+    <script src="{{ asset('module-assets/Auth/js/login.min.js?v=1') }}"></script>
+    <!-- end scripts -->
+    <script src="{{ asset('nanopkg-assets/js/tosterSession.min.js?v=1') }}"></script>
+</body>
+
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,87 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta
-      name="csrf-token"
-      content="N7IOTTFtITsRuDJjQFNQrIQJUOuoDcNi4Tlfh2uD"
-    />
-    <meta name="base-url" content="width=device-width, initial-scale=1" />
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <title>Login</title>
-    <meta name="description" content="Login" />
-    <link rel="canonical" href="login.html" />
-    <meta name="robots" content="all" />
-    <meta property="og:description" content="Login" />
-    <meta property="og:title" content="Login" />
-    <meta property="og:url" content="login.html" />
-    <meta property="og:type" content="WebPage" />
-    <meta property="og:site_name" content="vms-laravel" />
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="admin-assets/img/favicon.png?v=1" />
-    <link href="{{ asset('admin-assets/vendor/bootstrap/css/bootstrap.min.css?v=1')}}"
-    rel="stylesheet" /
-    <link
-      href="{{ asset('admin-assets/vendor/metisMenu/metisMenu.min.css?v=1') }}"
-      rel="stylesheet"
-    />
-    <link
-      href="{{ asset('nanopkg-assets/vendor/datatables/dataTables.bootstrap4.min.css?v=1') }}"
-      rel="stylesheet"
-    />
-    <link
-      href="{{ asset('admin-assets/vendor/typicons/src/typicons.min.css?v=1') }}"
-      rel="stylesheet"
-    />
-    <link
-      href="{{ asset('admin-assets/vendor/themify-icons/themify-icons.min.css?v=1') }}"
-      rel="stylesheet"
-    />
-    <link
-      href="{{ asset('admin-assets/vendor/material_icons/materia_icons.css?v=1') }}"
-      rel="stylesheet"
-    />
-    <link
-      href="{{ asset('admin-assets/vendor/emojionearea/dist/emojionearea.min.css?v=1') }}"
-      rel="stylesheet"
-    />
-    <link
-      rel="stylesheet"
-      href=" {{ asset('nanopkg-assets/vendor/highlight/highlight.min.css?v=1') }}"
-    />
-    <link
-      href="{{ asset('nanopkg-assets/vendor/sweetalert2/sweetalert2.min.css?v=1') }}"
-      rel="stylesheet"
-    />
-    <link
-      href="{{ asset('nanopkg-assets/vendor/fontawesome-free-6.3.0-web/css/all.min.css?v=1') }}"
-      rel="stylesheet"
-    />
-    <link
-      href="{{ asset('nanopkg-assets/vendor/bootstrap-icons/css/bootstrap-icons.min.css?v=1') }}"
-      rel="stylesheet"
-    />
-    <link
-      href="{{ asset('nanopkg-assets/vendor/toastr/build/toastr.min.css?v=1') }}"
-      rel="stylesheet"
-    />
-    <link href="{{ asset('nanopkg-assets/css/arrow-hidden.min.css?v=1') }}" rel="stylesheet" />
-    <link href="{{ asset('nanopkg-assets/css/custom.min.css?v=1') }}" rel="stylesheet" />
-
-    <!--Start Your Custom Style Now-->
-    <link href="{{ asset('admin-assets/css/style-new.min.css?v=1') }}" rel="stylesheet" />
-    <link href="{{ asset('admin-assets/css/custom.min.css?v=1') }}" rel="stylesheet" />
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
-    <link href="{{ asset('admin-assets/scss/customStyle.min.css?v=1') }}" rel="stylesheet" />
-    <link href="{{ asset('admin-assets/css/grapData.min.css?v=1') }}" rel="stylesheet" />
-
-    <link rel="stylesheet" href="{{ asset('module-assets/Auth/css/login.min.css?v=1') }}" />
-  </head>
 
   <body class="">
     <!-- Preloader -->
@@ -111,12 +29,7 @@
                 <p>Enter your email and password to sign in!</p>
               </div>
               <form class="register-form mt-3" method="POST" action="login">
-                <input
-                  type="hidden"
-                  name="_token"
-                  value="N7IOTTFtITsRuDJjQFNQrIQJUOuoDcNi4Tlfh2uD"
-                  autocomplete="off"
-                />
+                @csrf
                 <div class="mb-3">
                   <input
                     type="email"
@@ -235,30 +148,5 @@
     <!-- /.End of form wrapper -->
     <!-- start scripts -->
     <!--Global script(used by all pages)-->
-    <script src="{{ asset('admin-assets/vendor/jQuery/jquery.min.js?v=1') }}"></script>
-    <script src="{{ asset('admin-assets/vendor/bootstrap/js/bootstrap.bundle.min.js?v=1') }}"></script>
-    <script src="{{ asset('admin-assets/vendor/emojionearea/dist/emojionearea.min.js?v=1') }}"></script>
-
-    <script src="{{ asset('admin-assets/vendor/metisMenu/metisMenu.min.js?v=1') }}"></script>
-    <script src="{{ asset('admin-assets/vendor/perfect-scrollbar/dist/perfect-scrollbar.min.js?v=1') }}"></script>
-    <script src="{{ asset('nanopkg-assets/vendor/sweetalert2/sweetalert2.min.js?v=1') }}"></script>
-    <script src="{{ asset('nanopkg-assets/vendor/fontawesome-free-6.3.0-web/js/all.min.js?v=1') }}"></script>
-    <script src="{{ asset('nanopkg-assets/vendor/toastr/build/toastr.min.js?v=1') }}"></script>
-    <script src="{{ asset('nanopkg-assets/vendor/axios/dist/axios.min.js?v=1') }}"></script>
-    <script src="{{ asset('cnanopkg-assets/vendor/typed.js/lib/typed.min.js?v=1') }}"></script>
-    <script src=" {{ asset('nanopkg-assets/vendor/jquery-validation-1.19.5/jquery.validate.min.js?v=1') }}"></script>
-    <script src="{{ asset('cnanopkg-assets/js/axios.init.min.js?v=1') }}"></script>
-    <script src="{{ asset('nanopkg-assets/js/arrow-hidden.min.js?v=1') }}"></script>
-    <script src="{{ asset('nanopkg-assets/js/img-src.min.js?v=1') }}"></script>
-    <script src="{{ asset('nanopkg-assets/js/delete.min.js?v=1') }}"></script>
-    <script src="{{ asset('nanopkg-assets/js/user-status-update.min.js?v=1') }}"></script>
-    <script src="{{ asset('nanopkg-assets/js/main.js?v=1') }}"></script>
-
-    <!--Page Scripts(used by all page)-->
-    <script src="{{ asset('admin-assets/js/sidebar.min.js?v=1') }}"></script>
-    <script src="{{ asset('dmin-assets/js/custom.min.js?v=1') }}"></script>
-    <script src="{{ asset('module-assets/Auth/js/login.min.js?v=1') }}"></script>
-    <!-- end scripts -->
-    <script src="{{ asset('nanopkg-assets/js/tosterSession.min.js?v=1') }}"></script>
   </body>
 </html>
