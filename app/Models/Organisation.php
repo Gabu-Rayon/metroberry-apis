20b55d4c9731366c9a7bf7571ca3c1f0744a83ee
10-menachi <bobitlmr188221@spu.ac.ke>
2024-07-09 17:33:04 +0300

    Fix bug in code

--- app/Models/Organisation.php
+++ app/Models/Organisation.php
@@ -9,7 +9,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Organisation extends Model
 {
     use HasFactory;
+
     protected $table = 'organisations';
+    
     protected $fillable = [
         'user_id',
         'created_by',
