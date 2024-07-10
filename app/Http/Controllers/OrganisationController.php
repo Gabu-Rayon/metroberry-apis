5e7be839a16e4f69eabe384fbe763f00e127b036
10-menachi <bobitlmr188221@spu.ac.ke>
2024-07-10 15:56:01 +0300

    Create Edit Organisation View

--- app/Http/Controllers/OrganisationController.php
+++ app/Http/Controllers/OrganisationController.php
@@ -162,9 +162,9 @@ class OrganisationController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
-    {
-        //
+    public function edit(string $id) {
+        $organisation = Organisation::findOrfail($id);
+        return view('organisation.edit',compact('organisation'));
     }
 
     /**
